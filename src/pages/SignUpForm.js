5b3e5447418cd7f892ad54565f659d0da15d9cd3
aghabozorgi <faezeeaghabozorgi@gmail.com>
2019-08-12 14:52:58 +0430

    connect login

--- src/pages/SignUpForm.js
+++ src/pages/SignUpForm.js
@@ -1,20 +1,45 @@
 import React, {Component} from 'react';
-import Profile from "../components/ProfileNavBar";
+import './App.css';
+import { Redirect } from 'react-router-dom'
 
-class SignUpForm extends Component {
+class SignInForm extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            email: '',
+            username: '',
             password: '',
-            name: '',
-            hasAgreed: false
+            isLoggedIn:'',
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+    login(){
+         const formData = new FormData();
+         formData.set("username", this.state.username);
+         formData.set("password", this.state.password);
+        fetch('http://127.0.0.1:8000/accounts/login/',
+            {
+                method:"POST",
+                header: { "Content-Type": "multipart/form-data" },
 
+                body:formData
+            }
+
+            ).then((response) => {
+
+            console.log('success', response);
+            if (response.status === 200)
+             this.setState({
+              isLoggedIn: true
+            });
+            else{
+                console.log("user is not find")
+            }
+        }
+    )
+    }
     handleChange(e) {
         let target = e.target;
         let value = target.type === 'checkbox' ? target.checked : target.value;
@@ -27,51 +52,37 @@ class SignUpForm extends Component {
 
     handleSubmit(e) {
         e.preventDefault();
-        console.log('The form was submitted with the following data:');
-        console.log(this.state);
+        if (this.state.username === 'master' && this.state.password === 'master') {
+            this.history.pushState(null, 'sign-in');
+        }
     }
 
     render() {
+        if (this.state.isLoggedIn) {
+             return <Redirect to='./history' />
+        }
         return (
-            <div className='AppBar'>
-                <Profile emailAddress='emohamadhassan@gmail.com' firstName='Mohamad' lastName='Ebrahimi'
-                         isMaster={true}/>
-                <div className='App'>
-                    <div className='FormTitle'>
-                        <div className="FormCenter">
-                            <form onSubmit={this.handleSubmit} className="FormFields">
-                                <div className="FormField">
-                                    <label className="FormField__Label" htmlFor="name">Full Name</label>
-                                    <input type="text" id="name" className="FormField__Input"
-                                           placeholder="Enter your full name" name="name" value={this.state.name}
-                                           onChange={this.handleChange}/>
-                                </div>
-                                <div className="FormField">
-                                    <label className="FormField__Label" htmlFor="password">Password</label>
-                                    <input type="password" id="password" className="FormField__Input"
-                                           placeholder="Enter your password" name="password" value={this.state.password}
-                                           onChange={this.handleChange}/>
-                                </div>
-                                <div className="FormField">
-                                    <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                                    <input type="email" id="email" className="FormField__Input"
-                                           placeholder="Enter your email" name="email" value={this.state.email}
-                                           onChange={this.handleChange}/>
-                                </div>
+            <div className='App'>
+                <div className='FormTitle'>
+                    <div className="FormCenter">
+                        <form className="FormFields" onSubmit={this.handleSubmit}>
+                            <div className="FormField">
+                                <label className="FormField__Label" htmlFor="username">Username</label>
+                                <input type="username" id="username"  className="FormField__Input"
+                                       placeholder="Enter your username" name="username" value={this.state.username}
+                                       onChange={this.handleChange}/>
+                            </div>
 
-                                <div className="FormField">
-                                    <label className="FormField__CheckboxLabel">
-                                        <input className="FormField__Checkbox" type="checkbox" name="hasAgreed"
-                                               value={this.state.hasAgreed} onChange={this.handleChange}/> I agree all
-                                        statements in <a href="" className="FormField__TermsLink">terms of service</a>
-                                    </label>
-                                </div>
-
-                                <div className="FormField">
-                                    <button className="FormField__Button mr-20">Sign Up</button>
-                                </div>
-                            </form>
-                        </div>
+                            <div className="FormField">
+                                <label className="FormField__Label" htmlFor="password">Password</label>
+                                <input type="password" id="password" className="FormField__Input"
+                                       placeholder="Enter your password" name="password" value={this.state.password}
+                                       onChange={this.handleChange}/>
+                            </div>
+                            <div className="FormField">
+                                <button className="FormField__Button mr-20" onClick={()=>{this.login()}}>Sign In</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -79,8 +90,8 @@ class SignUpForm extends Component {
     }
 }
 
-export default SignUpForm;
+export default SignInForm;
 
 
 // WEBPACK FOOTER //
-// ./src/pages/SignUpForm.js
+// ./src/pages/SignInForm.js
